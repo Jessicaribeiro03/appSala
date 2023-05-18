619629ea776e50a0e0fb134a4cdc2a6bd1a01519
Jéssica Ribeiro Cruz <TRI0020060@aluno.firjansenaisesi.com.br>
2023-05-17 21:25:06 -0300

    atividade em aula

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.card}>
+        <Text style={styles.title}>Bem vindo ao Brasil</Text>
+        <Text style={styles.description}> is simply dumy text</Text>
+        <View style={styles.buttonView}>
+          <Button style={styles.button} tittle='Solicitar'/>
+        </View>
+
+      </View>
+      
+      <MapView style={styles.map} />
+    </View >
+
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  card: {
+    position:'absolut',
+    background:#FF69B4,
+    zIndex:1,
+    width: 100,
+    height: 100,
+    top:100,
+    left:100,
+  },
+
+  title: {
+    fontFamily: 'Arial',
+    top:30,
+    height:100,
+
+  },
+
+  description: {
+
+  },
+
+  buttonView:{
+    backgroundColor: #FFFF00,
+
+  }
+
+
+
 });
